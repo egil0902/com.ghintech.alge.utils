a8f4b0917964884610d5ee0884d74ef98df9204b
egil0902 <egil0902@gmail.com>
2020-07-22 12:38:16 -0500

    initial commit

---
+++ src/com/ghintech/alge/utils/process/ProcessFactory.java
@@ -0,0 +1,17 @@
+package com.ghintech.alge.utils.process;
+
+import org.adempiere.base.IProcessFactory;
+import org.compiere.process.ProcessCall;
+
+public class ProcessFactory implements IProcessFactory{
+
+	@Override
+	public ProcessCall newProcessInstance(String className) {
+		// TODO Auto-generated method stub
+		if(className.equals("com.ghintech.alge.utils.process.OrderBatchProcess"))
+			return new OrderBatchProcess();
+		
+		return null;
+	}
+
+}
