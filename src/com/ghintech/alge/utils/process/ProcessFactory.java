b2f9a0f08096a92d2cc52176710ce98f3d0c94c6
egil0902 <egil0902@gmail.com>
2020-11-30 09:44:28 -0500

    first commit

--- src/com/ghintech/alge/utils/process/ProcessFactory.java
+++ src/com/ghintech/alge/utils/process/ProcessFactory.java
@@ -8,8 +8,7 @@ public class ProcessFactory implements IProcessFactory{
 	@Override
 	public ProcessCall newProcessInstance(String className) {
 		// TODO Auto-generated method stub
-		if(className.equals("com.ghintech.alge.utils.process.OrderBatchProcess"))
-			return new OrderBatchProcess();
+	
 		if(className.equals("com.ghintech.alge.utils.process.BankRegister"))
 			return new BankRegister();
 		
